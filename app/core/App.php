83bd1b6f272feccd7de1d86dee00e7cca151d9af
zico <z.alamsyah1108@gmail.com>
2024-03-20 09:43:15 +0700

    Add Router Function

--- app/core/App.php
+++ app/core/App.php
@@ -1,6 +1,26 @@
 <?php
 class App {
   public function __construct() {
-    echo 'Berhasil masuk ke class utama';
+    
+    $url = $this->parseURL();
+    var_dump($url);
+  }
+
+  public function parseURL() {
+    if (isset($_GET['url'])) {
+      // Ambil Value dari URL
+      $url = $_GET['url'];
+
+      // Buang tanda / di akhir URL
+      $url = rtrim($url, '/');
+
+      // Filter URL dari karakter aneh
+      $url = filter_var($url, FILTER_SANITIZE_URL);
+      
+      // Pecah URL berdasarkan tanda / sebagai pemisah
+      $url = explode('/', $url);
+
+      return $url;
+    }
   }
 }
